fc907e1beed9e55d1921eb54bd5c15e5fa8c3b01
TonyKag <tonykag@mail.com>
2015-09-19 11:28:46 -0700

    scanner, input were implemented

--- src/GradeValue.java
+++ src/GradeValue.java
@@ -1,7 +1,4 @@
-/**
- * 
- */
-
+import java.util.Scanner;
 /**
  * @author Tony
  *
@@ -13,14 +10,37 @@ Create an application that asks the user for a grade value. Based on the value, 
 0-49        F
 * Also take in account incorrect value
 */
+import java.util.Scanner;
 public class GradeValue {
 
-	/**
-	 * @param args
-	 */
+// Create a Scanner member to be available through out class run
+//private static Scanner scanner;
+private static Scanner scanner;
+
 	public static void main(String[] args) {
+		//Variables to capture String of user input
+		String input;
+		// Converted grade value of user input string
+		int gradeValue;
+		
+		// introduction
+		System.out.println("[\tYou are using the Grade Value Application\t]");
+		// initialize the scanner
+		scanner = new Scanner(System.in);
+		//asks the user for a grade value
+		System.out.println("Please provide a grade value");
+		// store user input into a grade variable
+		input = scanner.nextLine();
+		// convert the string for input into a int value
+		gradeValue = Integer.parseInt(input);
+		
+		System.out.println("Grade:" + gradeValue);
+		
+		
+
+		
+		
 		
 
 	}
-
 }
