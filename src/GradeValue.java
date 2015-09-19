6af6bcda2e50d3fa784e132996d89290d092a37d
TonyKag <tonykag@mail.com>
2015-09-19 10:22:26 -0700

    empty class GradeValue created

---
+++ src/GradeValue.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+
+/**
+ * @author Tony
+ *
+Create an application that asks the user for a grade value. Based on the value, give back the letter grade corresponding to the value range. 
+80-100    A
+70-79      B
+60-69      C
+50-59      D
+0-49        F
+* Also take in account incorrect value
+*/
+public class GradeValue {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+
+	}
+
+}
